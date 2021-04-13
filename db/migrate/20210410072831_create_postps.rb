8b2ab3b3935f404c4e58f38b98e6e52592bb9fec
brayan alexander manrique bonilla <brayanalexandermanriquebonilla@hotmail.com>
2021-04-13 14:01:43 -0500

    correccion de migracion

--- db/migrate/20210410072831_create_postps.rb
+++ db/migrate/20210410072831_create_postps.rb
@@ -3,9 +3,9 @@ class CreatePostps < ActiveRecord::Migration[5.2]
     create_table :postps do |t|
       t.string :nombre
       t.string :contenido
-      t.integer :numerolikes
-      t.integer :numerolislikes
-      t.integer :numerocomentarios
+      t.integer :numero_likes
+      t.integer :numero_lislikes
+      t.integer :numero_comentarios
 
       t.timestamps
     end
