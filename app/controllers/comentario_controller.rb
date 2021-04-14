309fa88c11173589d04297dc63f8490ecc5f0840
brayan alexander manrique bonilla <brayanalexandermanriquebonilla@hotmail.com>
2021-04-14 11:40:55 -0500

    correccion order de aparicion de los posts

--- app/controllers/comentario_controller.rb
+++ app/controllers/comentario_controller.rb
@@ -11,8 +11,10 @@ class ComentarioController < ApplicationController
     def create 
 
         @comentary = Comentario.new(user_param)
+        @postId = Postp.where(postp_id: params[:postp_id])
         
         if @comentary.save 
+            @Post.update(numero_comentarios: @Post[0].numero_comentarios+1 )
             render json: { status: 'Success', message:'Comentary created', data:@comentary}, status: :ok
         else
             render json: { status: 'Error', message:'Error create Comentary', data:@comentary.errors}, status: :unprocessable_entity
